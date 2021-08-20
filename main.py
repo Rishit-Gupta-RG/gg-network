e432d717a4e0d9e1d19cd427f95d0e845657ad17
Rishit Gupta <gamingsandroking@gmail.com>
2021-08-20 21:33:54 +0530

    test 2 fix 1.3

--- main.py
+++ main.py
@@ -141,7 +141,7 @@ async def on_message(message):
     if not message.channel.id == 871006184990212118:
       return
     if "trade" or "trading" in message.content:
-        embed = discord.Embed(title= "Trading", description="The [trading](https://minecraft.fandom.com/wiki/Trading#Bedrock_Edition_offers) system is a gameplay mechanic that allows players to trade emeralds for items and vice-versa with villagers as well as wandering traders.\n""**There are 5 Tiers of trading:**\n""1. Novice\n""2. Apprentice\n""3. Journeyman\n""4. Expert\n""5. Master", color=discord.Colour.blurple)
+        embed = discord.Embed(title= "Trading", description="The [trading](https://minecraft.fandom.com/wiki/Trading#Bedrock_Edition_offers) system is a gameplay mechanic that allows players to trade emeralds for items and vice-versa with villagers as well as wandering traders.\n""**There are 5 Tiers of trading:**\n""1. Novice\n""2. Apprentice\n""3. Journeyman\n""4. Expert\n""5. Master", color=discord.Colour.blurple())
         embed.set_thumbnail(url=icon)
 
         await message.channel.send(embed=embed)
