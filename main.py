97c2e4d01b3c6e0db45455d7a902d4e0fe50732f
Rishit Gupta <83908451+Rishit-Gupta999@users.noreply.github.com>
2022-02-04 14:30:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ async def check(ctx):
     status = server.status()
     embed = disnake.Embed(title="Status of GG SMP", description=f" **Edition -** `Bedrock`\n **Version -** `1.18`\n **Players in game -** `{status.players_online}`\n **Maximum Players -** `{status.players_max}`", color=ctx.author.color)
     embed.set_thumbnail(url="https://media.discordapp.net/attachments/885185426741141504/921090028204085268/sjhnjkdbc.gif")
-    embed.set_footer(text="If Max players = 0 → Server is offline 🔴\nIf Max players = 20 → Server is online 🟢")
+    embed.set_footer(text="If Max players = 1 → Server is offline 🔴\nIf Max players = 20 → Server is online 🟢")
     await ctx.send(embed=embed)
 
 @bot.slash_command()
