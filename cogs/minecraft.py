cf6315973bb6fa33cb98ae1574a89c322c158e21
Rishit Gupta <gamingsandroking@gmail.com>
2022-03-09 14:57:54 +0530

    bruh momento

--- cogs/minecraft.py
+++ cogs/minecraft.py
@@ -16,11 +16,11 @@ class Minecraft(commands.Cog):
         server = MinecraftBedrockServer.lookup("ggnetworkk.aternos.me:34624")
         status = server.status()
         if status.players_max == '1':
-            off = disnake.Embed(title="Status for GG Network", description="Oh! no the server is offline \🔴\n\n Do you want to play now? Turn it thorugh [Aternos Dashboard](https://aternos.org/server/) or ask someone with <@&880915882895872080> role to turn it on.", color=ctx.author.color, timestamp=datetime.datetime.utcnow())
+            off = disnake.Embed(title="Status for GG Network", description="Oh! no the server is offline \🔴\n\n Do you want to play now? Turn it thorugh [Aternos Dashboard](https://aternos.org/server/) or ask someone with <@&880915882895872080> role to turn it on.", color=ctx.author.color, timestamp=datetime.utcnow())
             off.set_footer(icon_url=ctx.guild.icon, text=ctx.guild.name)
             await ctx.send(embed=off)
         else:
-            on = disnake.Embed(name="Status for GG Network", description=f"**Edition -** Bedrock:1.18.2\n**IP -** `ggnetworkk.aternos.me`\n**Port -** `34624`\n**Status -** Online \🟢\n**Online players -** `{status.players_online}`\n**Ping -** `{int(status.latency*100)}`", color=ctx.author.color, timestamp=datetime.datetime.utcnow())
+            on = disnake.Embed(name="Status for GG Network", description=f"**Edition -** Bedrock:1.18.2\n**IP -** `ggnetworkk.aternos.me`\n**Port -** `34624`\n**Status -** Online \🟢\n**Online players -** `{status.players_online}`\n**Ping -** `{int(status.latency*100)}`", color=ctx.author.color, timestamp=datetime.utcnow())
             on.set_footer(icon_url=ctx.guild.icon, text=ctx.guild.name)
             on.set_thumbnail(url=ctx.guild.icon)
             await ctx.send(embed=on)
