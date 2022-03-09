2f06dc8cff862e8e7a5955db3d2c634fb17d624b
Rishit Gupta <gamingsandroking@gmail.com>
2022-03-09 15:09:03 +0530

    k

--- cogs/minecraft.py
+++ cogs/minecraft.py
@@ -11,7 +11,7 @@ import random
 class Minecraft(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    @commands.command(name="check", description="Checks status for GG Network", breif="Want to check whether the server is online or not? just type the command an you'll get some information.", enabled=True)
+    @commands.command(name="check", brief="Checks status for GG Network", help="Want to check whether the server is online or not? just type the command an you'll get some information.", enabled=True)
     async def check(self, ctx):
         server = MinecraftBedrockServer.lookup("ggnetworkk.aternos.me:34624")
         status = server.status()
@@ -20,8 +20,9 @@ class Minecraft(commands.Cog):
             off.set_footer(icon_url=ctx.guild.icon, text=ctx.guild.name)
             await ctx.send(embed=off)
         else:
-            on = disnake.Embed(title="Status for GG Network", description=f"**Edition -** Bedrock:1.18.2\n**IP -** `ggnetworkk.aternos.me`\n**Port -** `34624`\n**Status -** Online \🟢\n**Online players -** `{status.players_online}`\n**Ping -** `{int(status.latency*100)}`", color=ctx.author.color, timestamp=datetime.utcnow())
+            on = disnake.Embed(title="Status for GG Network", description=f"**Status -** Online \🟢\n**Online players -** `{status.players_online}`\n**Ping -** `{int(status.latency*100)}ms`", color=ctx.author.color, timestamp=datetime.utcnow())
             on.set_footer(icon_url=ctx.guild.icon, text=ctx.guild.name)
+            on.set_author(name="ggnetwork.aternos.me")
             on.set_thumbnail(url=ctx.guild.icon)
             await ctx.send(embed=on)
 
