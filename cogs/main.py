eaf1eb7da9493e531ac22b020d91adedbaf5181b
Rishit Gupta <83908451+Rishit-Gupta-RG@users.noreply.github.com>
2022-04-13 02:09:12 +0530

    Update main.py

--- cogs/main.py
+++ cogs/main.py
@@ -65,14 +65,14 @@ class Refresh(disnake.ui.View):
         server = BedrockServer.lookup(data["server_ip"])
         status = server.status()
         if status.players_max == 1:
-            off = disnake.Embed(title=data["minecraft_server_name"] + "status panel.", description="**Status -** Offline :red_circle:\n\nDo you want to play now? Turn it on thorugh [Aternos Dashboard](https://aternos.org/server/) or ask someone with <@880915882895872080> role to turn it on.", color=0xf80000)
-            off.set_thumbnail(url=data["thumbnail_url"])
+            off = disnake.Embed(title="GG Network status panel.", description="**Status -** Offline :red_circle:\n\nDo you want to play now? Turn it on thorugh [Aternos Dashboard](https://aternos.org/server/) or ask someone with <@&880915882895872080> role to turn it on.", color=0xf80000)
+            off.set_thumbnail(url="https://cdn.discordapp.com/icons/817003562663149578/a_427636e6c26d830bbcc36969a9e83608.gif?size=4096")
             off.set_author(icon_url="thumbnail_url", name=data["server_ip"])
             off.set_footer(text='Click on the refresh button below to refresh the status.')
             await interaction.message.edit(embed=off)
         else:
-            on = disnake.Embed(title=data["minecraft_server_name"]+"status panel.", description=f"**Status -** Online :green_circle:\n\n**Online Players-** `{status.players_online}`\n**Max. Players -** `{status.players_max}`\n**Edition -** "+data["minecraft_version"]+f"\n**Ping -** `{int(status.latency*100)}ms`", color=0x3cff00)
-            on.set_thumbnail(url=data["thumbnail_url"])
+            on = disnake.Embed(title="GG Network status panel.", description=f"**Status -** Online :green_circle:\n\n**Online Players-** `{status.players_online}`\n**Max. Players -** `{status.players_max}`\n**Edition -** "+data["minecraft_version"]+f"\n**Ping -** `{int(status.latency*100)}ms`", color=0x3cff00)
+            on.set_thumbnail(url="https://cdn.discordapp.com/icons/817003562663149578/a_427636e6c26d830bbcc36969a9e83608.gif?size=4096")
             on.set_footer(text='Click on the refresh button below to refresh the status.')
             on.set_author(icon_url=data["thumbnail_url"], name=data["server_ip"])
             await interaction.message.edit(embed=on)
@@ -81,7 +81,7 @@ class Refresh(disnake.ui.View):
 @bot.command(hidden=True, description='deploys status checker.')
 async def deploy(ctx):
     view = Refresh()
-    off = disnake.Embed(title="Status for"+data["minecraft_server_name"], description="Oh! no the server is offline \🔴\n\n Do you want to play now? Turn it on thorugh [Aternos Dashboard](https://aternos.org/server/) or ask someone with <@880915882895872080> role to turn it on.", color=ctx.author.color)
+    off = disnake.Embed(title="Status for GG Network", description="Oh! no the server is offline \🔴\n\n Do you want to play now? Turn it on thorugh [Aternos Dashboard](https://aternos.org/server/) or ask someone with <@&880915882895872080> role to turn it on.", color=ctx.author.color)
     off.set_footer(icon_url=ctx.guild.icon, text=ctx.guild.name)
     await ctx.send(embed=off, view=view)
 
